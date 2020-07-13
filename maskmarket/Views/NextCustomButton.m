165afc96c88c3ff5637eaf8fc42cabdec5ed2e60
alexoseg <aoseg@stanford.edu>
2020-07-13 14:18:38 -0700

    Added a custom button class

---
+++ maskmarket/Views/NextCustomButton.m
@@ -0,0 +1,21 @@
+//
+//  NextCustomButton.m
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/13/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "NextCustomButton.h"
+
+@implementation NextCustomButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
