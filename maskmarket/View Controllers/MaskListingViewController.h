7bdba5953e6272fe9e13c1b27228a1431a6c2a4d
alexoseg <aoseg@stanford.edu>
2020-07-13 15:53:53 -0700

    Added ability to logout of account

---
+++ maskmarket/View Controllers/MaskListingViewController.h
@@ -0,0 +1,17 @@
+//
+//  MaskListingViewController.h
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/13/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MaskListingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ maskmarket/View Controllers/MaskListingViewController.m
@@ -0,0 +1,32 @@
+//
+//  MaskListingViewController.m
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/13/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "MaskListingViewController.h"
+
+@interface MaskListingViewController ()
+
+@end
+
+@implementation MaskListingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
