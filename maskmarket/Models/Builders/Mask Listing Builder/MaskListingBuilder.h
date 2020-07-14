e27fe28de69c3530f347b45479d793768e4716d3
alexoseg <aoseg@stanford.edu>
2020-07-14 15:53:31 -0700

    Updated MaskListingBuilder to be used to build a listing through the creation screens

--- maskmarket/Models/Builders/Mask Listing Builder/MaskListingBuilder.h
+++ maskmarket/Models/Builders/Mask Listing Builder/MaskListingBuilder.h
@@ -14,11 +14,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MaskListingBuilder : NSObject
 
+@property (nonatomic, strong) NSString *listingTitle;
+@property (nonatomic, strong) UIImage *listingImage;
+@property (nonatomic, strong) NSString *listingCity;
+@property (nonatomic, strong) NSString *listingState;
+@property (nonatomic, strong) NSString *listingDescription;
+@property (nonatomic, strong) NSNumber *listingPrice;
+
 + (nullable MaskListing *)buildMaskListingFromPFObject:(PFObject *)object;
 
 + (instancetype)new NS_UNAVAILABLE;
-
-- (instancetype)init NS_UNAVAILABLE;
 
 @end
 
