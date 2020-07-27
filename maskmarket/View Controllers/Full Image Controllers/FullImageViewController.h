4d20466633a8421472d5e7bcb42be2323d6f19f7
alexoseg <aoseg@stanford.edu>
2020-07-27 13:59:05 -0700

    Added View Controller for new Full Image Screen

---
+++ maskmarket/View Controllers/Full Image Controllers/FullImageViewController.h
@@ -0,0 +1,17 @@
+//
+//  FullImageViewController.h
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/27/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FullImageViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ maskmarket/View Controllers/Full Image Controllers/FullImageViewController.m
@@ -0,0 +1,21 @@
+//
+//  FullImageViewController.m
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/27/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "FullImageViewController.h"
+
+#pragma mark - Interface
+
+@interface FullImageViewController ()
+
+@end
+
+#pragma mark - Implementation
+
+@implementation FullImageViewController
+
+@end
