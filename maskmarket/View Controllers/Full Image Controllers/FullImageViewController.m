21485b43830f5de917dba0c4b07e7a954016061b
alexoseg <aoseg@stanford.edu>
2020-07-27 14:08:41 -0700

    The Full Image screen now displays passed in image

--- maskmarket/View Controllers/Full Image Controllers/FullImageViewController.m
+++ maskmarket/View Controllers/Full Image Controllers/FullImageViewController.m
@@ -12,10 +12,29 @@
 
 @interface FullImageViewController ()
 
+#pragma mark - Properties
+
+@property (weak, nonatomic) IBOutlet UIImageView *maskImageView;
+
 @end
 
 #pragma mark - Implementation
 
 @implementation FullImageViewController
+
+#pragma mark - Lifecycles
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setUpViews];
+}
+
+#pragma mark - Setup
+
+- (void)setUpViews
+{
+    [_maskImageView setImage:_maskImage];
+}
 
 @end
