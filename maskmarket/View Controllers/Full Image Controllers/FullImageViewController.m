5438b67e3f907337b839f2b617e8558999e320d9
alexoseg <aoseg@stanford.edu>
2020-07-27 14:24:04 -0700

    Added code for the pinch gesture recognizer

--- maskmarket/View Controllers/Full Image Controllers/FullImageViewController.m
+++ maskmarket/View Controllers/Full Image Controllers/FullImageViewController.m
@@ -30,6 +30,41 @@
     [self setUpViews];
 }
 
+#pragma mark - Gesture Recognizers
+
+- (IBAction)onPinchImage:(UIPinchGestureRecognizer *)sender
+{
+    if (sender.state == UIGestureRecognizerStateBegan
+        || sender.state == UIGestureRecognizerStateChanged )
+    {
+        CGFloat const currentScale = self.maskImageView.frame.size.width / self.maskImageView.bounds.size.width;
+        CGFloat newScale = currentScale * sender.scale;
+        
+        if (newScale < 1.0) {
+            newScale = 1.0;
+        }
+        
+        if (newScale > 2.0) {
+            newScale = 2.0;
+        }
+        
+        self.maskImageView.transform = CGAffineTransformMakeScale(newScale, newScale);
+        sender.scale = 1;
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        typeof(self) __weak weakSelf = self;
+        [UIView animateWithDuration:0.3
+                         animations:^{
+            typeof(weakSelf) strongSelf = weakSelf;
+            if (weakSelf == nil) {
+                return;
+            }
+            
+            strongSelf.maskImageView.transform = CGAffineTransformIdentity;
+        }];
+    }
+}
+
+
 #pragma mark - Setup
 
 - (void)setUpViews
