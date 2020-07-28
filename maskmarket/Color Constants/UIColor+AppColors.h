ee958ba1593c9a52da8365946f756e170cf3e3e0
alexoseg <aoseg@stanford.edu>
2020-07-27 19:19:33 -0700

    Added empty color constants file

---
+++ maskmarket/Color Constants/UIColor+AppColors.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+AppColors.h
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/27/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (AppColors)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ maskmarket/Color Constants/UIColor+AppColors.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+AppColors.m
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/27/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "UIColor+AppColors.h"
+
+@implementation UIColor (AppColors)
+
+@end
