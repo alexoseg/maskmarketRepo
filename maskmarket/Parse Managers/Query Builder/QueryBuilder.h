c59103bcf93a30d19a69166bd12e6ce0d0ba20d6
alexoseg <aoseg@stanford.edu>
2020-07-27 18:06:02 -0700

    Created empty query builder class

---
+++ maskmarket/Parse Managers/Query Builder/QueryBuilder.h
@@ -0,0 +1,17 @@
+//
+//  QueryBuilder.h
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/27/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QueryBuilder : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ maskmarket/Parse Managers/Query Builder/QueryBuilder.m
@@ -0,0 +1,13 @@
+//
+//  QueryBuilder.m
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/27/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "QueryBuilder.h"
+
+@implementation QueryBuilder
+
+@end
