d9b5dadf8b27795a824f84a912bc73dff6a0291b
alexoseg <aoseg@stanford.edu>
2020-07-29 09:23:49 -0700

    Added cancel button to the error popup view

--- maskmarket/Views/Error Popup View/ErrorPopupView.m
+++ maskmarket/Views/Error Popup View/ErrorPopupView.m
@@ -12,15 +12,18 @@
 @implementation ErrorPopupView
 
 - (instancetype)initWithMessage:(NSString *)message
+                      addCancel:(BOOL)addCancel
 {
-    self = [super initWithFrame:CGRectZero];
+    self = [super initWithFrame:CGRectMake(0, 0, 250.0f, 250.0f)];
     if (self) {
-        [self setUpViewsWithMessage:message];
+        [self setUpViewsWithMessage:message
+                          addCancel:addCancel];
     }
     return self;
 }
                    
 - (void)setUpViewsWithMessage:(NSString *)message
+                    addCancel:(BOOL)addCancel
 {
     self.translatesAutoresizingMaskIntoConstraints = NO;
     self.backgroundColor = [UIColor primaryAppColor];
@@ -69,9 +72,47 @@
     [_tryAgainButton.heightAnchor constraintEqualToConstant:40].active = YES;
     [_tryAgainButton.topAnchor constraintGreaterThanOrEqualToAnchor:messageLabel.bottomAnchor
                                                           constant:15].active = YES;
-    [_tryAgainButton.leadingAnchor constraintEqualToAnchor:self.leadingAnchor].active = YES;
     [_tryAgainButton.bottomAnchor constraintEqualToAnchor:self.bottomAnchor].active = YES;
     [_tryAgainButton.trailingAnchor constraintEqualToAnchor:self.trailingAnchor].active = YES;
+    
+    if (!addCancel) {
+        [_tryAgainButton.leadingAnchor constraintEqualToAnchor:self.leadingAnchor].active = YES;
+        UIView *const topBorderLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width, 1.0f)];
+        topBorderLine.backgroundColor = [UIColor popUpViewBackgroundAlphaHalf];
+        [_tryAgainButton addSubview:topBorderLine];
+        return;
+    }
+    
+    _cancelButton = [UIButton new];
+    _cancelButton.translatesAutoresizingMaskIntoConstraints = NO;
+    _cancelButton.backgroundColor = [UIColor popUpViewBackgroundAlphaHalf];
+    [_cancelButton setTitle:@"Cancel"
+                   forState:UIControlStateNormal];
+    [_cancelButton setTitleColor:UIColor.whiteColor
+                        forState:UIControlStateNormal];
+    _cancelButton.titleLabel.font = [UIFont fontWithName:@"Montserrat-Regular" size:14.0f];
+    _cancelButton.userInteractionEnabled = YES;
+    [self addSubview:_cancelButton];
+    
+    [_cancelButton.leadingAnchor constraintEqualToAnchor:self.leadingAnchor].active = YES;
+    [_cancelButton.bottomAnchor constraintEqualToAnchor:self.bottomAnchor].active = YES;
+    [_cancelButton.heightAnchor constraintEqualToConstant:40].active = YES;
+    [_cancelButton.trailingAnchor constraintEqualToAnchor:_tryAgainButton.leadingAnchor
+                                                 constant:0].active = YES;
+    [_cancelButton.widthAnchor constraintEqualToAnchor:self.widthAnchor
+                                            multiplier:0.5].active = YES;
+
+    UIView *const centerBorderLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1.0f, 40.0f)];
+    centerBorderLine.backgroundColor = [UIColor popUpViewBackgroundAlphaHalf];
+    [_tryAgainButton addSubview:centerBorderLine];
+    
+    UIView *const topRightBorderLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width / 2, 1.0f)];
+    topRightBorderLine.backgroundColor = [UIColor popUpViewBackgroundAlphaHalf];
+    [_tryAgainButton addSubview:topRightBorderLine];
+
+    UIView *const topLeftBorderLine = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.frame.size.width / 2, 1.0f)];
+    topLeftBorderLine.backgroundColor = [UIColor popUpViewBackgroundAlphaHalf];
+    [_cancelButton addSubview:topLeftBorderLine];
 }
 
 @end
