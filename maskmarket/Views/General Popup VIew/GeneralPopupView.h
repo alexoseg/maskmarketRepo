6f1589a72c9c59f24d18c7c4f7fbe65855b56fe1
alexoseg <aoseg@stanford.edu>
2020-07-29 14:38:47 -0700

    Added function definition for a success popup view

--- maskmarket/Views/General Popup VIew/GeneralPopupView.h
+++ maskmarket/Views/General Popup VIew/GeneralPopupView.h
@@ -20,6 +20,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)initErrorPopupWithMessage:(NSString *)message
                                 addCancel:(BOOL)addCancel;
 
+- (instancetype)initSuccessPopupWithMessage:(NSString *)message;
+
 - (instancetype)init NS_UNAVAILABLE;
 
 + (instancetype)new NS_UNAVAILABLE;
