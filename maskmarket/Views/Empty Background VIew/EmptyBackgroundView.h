bdf2e4976d62eca8af17ba7507fe65203668b4b2
alexoseg <aoseg@stanford.edu>
2020-07-30 13:12:26 -0700

    Added initializers to the empty background view header

--- maskmarket/Views/Empty Background VIew/EmptyBackgroundView.h
+++ maskmarket/Views/Empty Background VIew/EmptyBackgroundView.h
@@ -12,6 +12,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface EmptyBackgroundView : UIView
 
+- (instancetype)initWithImageName:(NSString *)imageName
+                            title:(NSString *)title
+                          message:(NSString *)message; 
+
+- (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)new NS_UNAVAILABLE;
+
 @end
 
 NS_ASSUME_NONNULL_END
