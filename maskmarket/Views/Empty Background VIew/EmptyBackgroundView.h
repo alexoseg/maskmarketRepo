b5d111c3affd6255753a88db1922444dcbcf7a24
alexoseg <aoseg@stanford.edu>
2020-07-30 11:24:23 -0700

    Added the Empty Background View class

---
+++ maskmarket/Views/Empty Background VIew/EmptyBackgroundView.h
@@ -0,0 +1,17 @@
+//
+//  EmptySellingView.h
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/30/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EmptyBackgroundView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ maskmarket/Views/Empty Background VIew/EmptyBacklgroundView.m
@@ -0,0 +1,14 @@
+//
+//  EmptySellingView.m
+//  maskmarket
+//
+//  Created by Alex Oseguera on 7/30/20.
+//  Copyright © 2020 Alex Oseguera. All rights reserved.
+//
+
+#import "EmptySellingView.h"
+
+@implementation EmptyBackgroundView
+
+
+@end
