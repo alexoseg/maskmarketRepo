e9087fb24a2c120e34cb5cae1f2f6096869f71d1
alexoseg <aoseg@stanford.edu>
2020-08-05 14:31:20 -0700

    Sale Completion Controller now pulls the users shipping info

--- maskmarket/View Controllers/Sale Completion Controllers/SaleCompletionViewController.m
+++ maskmarket/View Controllers/Sale Completion Controllers/SaleCompletionViewController.m
@@ -12,6 +12,7 @@
 #import "UIColor+AppColors.h"
 #import "ErrorPopupViewController.h"
 #import "SuccessPopupViewController.h"
+#import "ParseGetter.h"
 
 #pragma mark - Interface
 
@@ -25,8 +26,17 @@
 @property (weak, nonatomic) IBOutlet UILabel *quantityLabel;
 @property (weak, nonatomic) IBOutlet UITextField *trackingTextField;
 @property (strong, nonatomic) UIButton *completePurchaseButton;
+@property (weak, nonatomic) IBOutlet UILabel *streetAddressLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cityStateZipLabel;
 
 @end
+
+#pragma mark - Constants
+
+static NSString *const kShippingStreetAddress = @"shippingStreetAddress";
+static NSString *const kShippingZipCode = @"shippingZipCode";
+static NSString *const kShippingCity = @"shippingCity";
+static NSString *const kShippingState = @"shippingState";
 
 #pragma mark - Implementation
 
@@ -38,6 +48,30 @@
 {
     [super viewDidLoad];
     [self setUpViews];
+}
+
+#pragma mark - Networking
+
+- (void)fetchShippingInfo
+{
+    [LoadingPopupView showLoadingPopupAddedTo:self.view
+                                  withMessage:@"Loading..."];
+    NSString *const userID = _boughListing.buyerID;
+    typeof(self) __weak weakSelf = self;
+    [ParseGetter fetchUserWithID:userID
+                  withCompletion:^(PFObject * _Nullable object, NSError * _Nullable error) {
+        typeof(weakSelf) strongSelf = weakSelf;
+        if (strongSelf == nil) {
+            return;
+        }
+        
+        [LoadingPopupView hideLoadingPopupAddedTo:strongSelf.view];
+        strongSelf.streetAddressLabel.text = object[kShippingStreetAddress];
+        NSString *const city = object[kShippingCity];
+        NSString *const state = object[kShippingState];
+        NSString *const zipCode = object[kShippingZipCode];
+        strongSelf.cityStateZipLabel.text = [NSString stringWithFormat:@"%@, %@ %@", city, state, zipCode];
+    }];
 }
 
 #pragma mark - Gesture Recognizers
@@ -97,6 +131,7 @@
 
 - (void)setUpViews
 {
+    [self fetchShippingInfo];
     _maskTitleLabel.text = _boughListing.title;
     _quantityLabel.text = [NSString stringWithFormat:@"%d", _boughListing.maskQuantity];
     
